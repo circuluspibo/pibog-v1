344d607dc09c2c1768f7377138c3e10fca84e6ef
circulus <50613069+circuluspibo@users.noreply.github.com>
2025-06-17 10:44:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ core = ov.Core()
 
 det_ov_model = core.read_model('yolo12m_int8_openvino_model/yolo12m.xml')
 det_model = YOLO('yolo12m_int8_openvino_model', task='detect')
-sam_model = FastSAM("FastSAM-s_int8_openvino_model")  # or FastSAM-x.pt
+sam_model = FastSAM("./FastSAM-s_int8_openvino_model")  # or FastSAM-x.pt
 
 det_ov_model.reshape({0: [1, 3, 384, 640]})
 compiled_model = core.compile_model(det_ov_model, 'NPU')
@@ -73,6 +73,19 @@ if det_model.predictor is None:
     det_model.predictor.setup_model(model=det_model.model)
 
 det_model.predictor.model.ov_compiled_model = compiled_model
+
+"""
+wrapped_model = OVWrapper(
+    "FastSAM-s_int8_openvino_model/FastSAM-s.xml",
+    device="NPU",
+    #stride=sam_model.predictor.model.stride,
+    #ov_config=ov_config,
+)
+
+print(sam_model.keys())
+
+sam_model.predictor.model.model = wrapped_model
+"""
 
 # Enable logging for debugging
 logging.basicConfig(level=logging.ERROR)
@@ -410,7 +423,7 @@ async def video_feed():
 
                 start_time = time.time()
                 results = det_model(image, verbose=False)[0]
-                result = sam_model(image, device="NPU",retina_masks=True, imgsz=1024, conf=0.8, iou=0.9, texts=['road'])[0] 
+                result = sam_model(image, device="intel:npu", verbose=False, retina_masks=True, imgsz=640, conf=0.6, iou=0.9)[0] # texts=['road'] 
                 stop_time = time.time()
 
 
@@ -453,11 +466,11 @@ async def video_feed():
                 state["cnt_live"] = cnt_live
                 state["cnt_object"] = cnt_object
 
-                frame = output#.results.plot()
+                #frame = output#.results.plot()
                 ## road detect
                 masks = result.masks.data.cpu().numpy()  # (N, H, W)
 
-                print(result)
+                #print(result)
 
                 # 복사본 만들기 (출력용)
                 #overlay = img.copy()
@@ -467,6 +480,12 @@ async def video_feed():
                 alpha = 0.5               # 투명도
 
                 for mask in masks:
+                    colored_mask = (mask * 255).astype(np.uint8) * 255
+                    # 윤곽선 그리기 (선택 사항)
+                    contours, _ = cv2.findContours(colored_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    cv2.drawContours(output, contours, -1, (0, 255, 0), 3)                    
+
+                    """
                     colored_mask = (mask * 255).astype(np.uint8)
 
                     # 컬러 마스크 생성
@@ -483,7 +502,8 @@ async def video_feed():
                     # 윤곽선 그리기 (선택 사항)
                     contours, _ = cv2.findContours(colored_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                     cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
-
+                    """
+                """
                 road_mask = result.masks.data[0].cpu().numpy()  # shape: (H, W)
 
                 # === 4. YOLO - 객체 감지 후 obstacle mask 생성 ===
@@ -519,7 +539,7 @@ async def video_feed():
                     pt1 = (int(x_smooth[i - 1]), int(y_smooth[i - 1]))
                     pt2 = (int(x_smooth[i]), int(y_smooth[i]))
                     cv2.line(frame, pt1, pt2, (255, 0, 0), 2)  
-
+                """
                 processing_times.append(stop_time - start_time)
 
                 if len(processing_times) > 200:
@@ -531,7 +551,7 @@ async def video_feed():
                 fps = 1000 / processing_time
                 
                 cv2.putText(
-                  img=frame,
+                  img=output,
                   text=f"Inference time: {processing_time:.1f}ms ({fps:.1f} FPS)",
                   org=(20, 40),
                   fontFace=cv2.FONT_HERSHEY_COMPLEX,
