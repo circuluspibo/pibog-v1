590cf07fa86a5aea3c8dbb00406c651753a24246
rippertnt <rippertnt@circul.us>
2025-10-30 17:04:02 +0900

    update

--- index_cpu.py
+++ index_cpu.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':    
-    uvicorn.run("main_cpu_mcr:app",host="0.0.0.0",port=59530,reload=False)
+    uvicorn.run("main_cpu:app",host="0.0.0.0",port=59530,reload=False)
