14d8cfb2c944d9595e535ac39df54e728e3eca4d
circulus <50613069+circuluspibo@users.noreply.github.com>
2025-11-22 11:57:50 +0900

    Add inference threading to TTS model configuration

--- main_cpu.py
+++ main_cpu.py
@@ -56,7 +56,7 @@ app.add_middleware(
 
 core = Core()
 path_tts = snapshot_download(repo_id="rippertnt/on-vits2-multi-tts-v1", allow_patterns="*ov*")
-pipe_tts = core.compile_model(core.read_model(model=f"{path_tts}/all_base_ov.xml"), device_name="CPU", config={"PERFORMANCE_HINT": "LATENCY"})
+pipe_tts = core.compile_model(core.read_model(model=f"{path_tts}/all_base_ov.xml"), device_name="CPU", config={"PERFORMANCE_HINT": "LATENCY", "INFERENCE_NUM_THREADS" : 4 })
 conf_tts = utils.get_hparams_from_file(hf_hub_download(repo_id="rippertnt/on-vits2-multi-tts-v1", filename="all_base.json"))
 
 
