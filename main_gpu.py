f71ce8fa1c4d8754ed554764f1542b4b41ca2671
circulus <50613069+circuluspibo@users.noreply.github.com>
2025-12-01 07:52:18 +0900

    Update main_gpu.py

--- main_gpu.py
+++ main_gpu.py
@@ -116,9 +116,10 @@ config = {
     "GPU_HOST_TASK_PRIORITY": "HIGH"
 }
 
+pipe_stt = ov_genai.WhisperPipeline(model_stt,device="GPU", config={"PERFORMANCE_HINT": "LATENCY"})
+
 token_txt = AutoTokenizer.from_pretrained(model_txt)
 pipe_txt = ov_genai.VLMPipeline(model_txt, device="GPU", config={"PERFORMANCE_HINT": "LATENCY"})
-pipe_stt = ov_genai.WhisperPipeline(model_stt,device="GPU", config={"PERFORMANCE_HINT": "LATENCY"})
 
 # for genai
 async def process_stream(streamer, isStream=True, isPlay=0, lang='en'):
