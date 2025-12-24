8dfe533b85ae411f99908e1f5d559872b71a533f
circulus <50613069+circuluspibo@users.noreply.github.com>
2025-12-25 01:27:34 +0900

    Change app reference in uvicorn.run call

--- index_npu_basic2.py
+++ index_npu_basic2.py
@@ -2,4 +2,4 @@
 import uvicorn
 
 if __name__ == '__main__':    
-    uvicorn.run("main_npu_basic:app",host="0.0.0.0",port=59531,reload=False)
+    uvicorn.run("main_npu_basic2:app",host="0.0.0.0",port=59531,reload=False)
