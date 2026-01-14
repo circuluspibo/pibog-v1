c9ddaf5a03b102a8bce37b5dea4b5adc1d10092f
rippertnt <rippertnt@circul.us>
2026-01-14 12:35:25 +0900

    update

--- index_npu_sr.py
+++ index_npu_sr.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':    
-    uvicorn.run("main_npu:app",host="0.0.0.0",port=59531,reload=False)
+    uvicorn.run("main_npu_sr:app",host="0.0.0.0",port=59531,reload=False)
