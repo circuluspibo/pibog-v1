2fe106ee7b796e6d742e0466009afbe3f53cb637
circulus <50613069+circuluspibo@users.noreply.github.com>
2026-05-09 19:52:23 -0400

    Update main_gpu.py

--- main_gpu.py
+++ main_gpu.py
@@ -254,7 +254,7 @@ async def process_stream(streamer, isStream=True, isPlay=0, lang='en', voice=6):
                 sentence = sentence.strip()
 
                 if int(isPlay) > 0:
-                    get("http://127.0.0.1:59530/v1/tts",params={"text": sentence, "lang": lang, "voice": voice})
+                    get("http://127.0.0.1:59530/v1/tts",params={"text": sentence, "lang": lang, "voice": voice, "isPlay" : 1})
 
                 print(sentence)
                 yield sentence
@@ -267,7 +267,7 @@ async def process_stream(streamer, isStream=True, isPlay=0, lang='en', voice=6):
     # 마지막 문장 처리
     if len(sentence) > 3:
         if int(isPlay) > 0:
-            get( "http://127.0.0.1:59530/v1/tts",params={"text": sentence, "lang": lang, "voice": voice})        
+            get( "http://127.0.0.1:59530/v1/tts",params={"text": sentence, "lang": lang, "voice": voice, "isPlay" : 1})        
         yield sentence
         await asyncio.sleep(0)
 
